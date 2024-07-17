370a68769f52ce3a6283bca87f09356963e6434e
EvgenyKushvid <nlmontreal24@gmail.com>
2024-07-17 03:09:59 +0300

    Add files via upload

--- homework5.py
+++ homework5.py
@@ -1,6 +1,7 @@
 immutable_var= ("String", 23, True)
 print('immutable tuple ',immutable_var)
 #immutable_var [0]= 'Name'
+# тип кортеж относится к неизменяемым типам данных
 mutable_list = ['List', True,45]
 mutable_list[2] = "Modified"
 print("Modified list",mutable_list)
