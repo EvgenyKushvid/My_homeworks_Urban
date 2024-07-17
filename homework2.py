b1c48acb46aefad07c36c70e5ebd3c0f5f387d40
EvgenyKushvid <nlmontreal24@gmail.com>
2024-07-17 03:00:27 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+homeworks = 12
+houers = 1.5
+course_name = 'Phython'
+time_one_exercise =houers/homeworks
+print('Курс',course_name,'всего задач: ',homeworks,'затрачено часов: '
+      ,houers, 'среднее время выполнения ',time_one_exercise, 'часа')
