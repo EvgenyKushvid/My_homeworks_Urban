138a7b21a048847951740eec347304874a17c6d3
EvgenyKushvid <nlmontreal24@gmail.com>
2024-07-17 03:00:05 +0300

    Delete homework2.py

--- homework2.py
+++
@@ -1,6 +0,0 @@
-homeworks = 12
-houers = 1.5
-course_name = 'Phython'
-time_one_exercise =houers/homeworks
-print('Курс',course_name,'всего задач: ',time_one_exercise,'затрачено часов: '
-      ,houers, 'среднее времчч выполнения: ',time_one_exercise)
