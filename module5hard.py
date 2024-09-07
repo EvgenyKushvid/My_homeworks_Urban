4d4407835c9c48e8fdbe9a298610aa9090bacd79
EvgenyKushvid <nlmontreal24@gmail.com>
2024-09-07 13:26:09 +0300

    Add files via upload

--- module5hard.py
+++ module5hard.py
@@ -3,17 +3,18 @@ import time
 class User:
     def __init__(self, nickname, password,age):
         self.nickname = nickname
-        self.password = hash(password)
+        self.password = password
         self.age = age
-
+    def __eq__(self, other):
+        return other.nickname == self.nickname
     def __str__ (self):
         return self.nickname
 
-    def __contains__(self, item):
-        for obj in ur.users:
-            if item.nickname == obj.nickname:
-                return True
-        #return any(item.nickname == obj.nickname for obj in ur.users)
+    # def __hash__(self):
+    #     return hash(self.password)
+    def get_info (self):
+        return self.nickname,hash(self.password)
+
 
 class Video:
     def __init__(self, title, duration, time_now = 0, adult_mode=False):
@@ -22,65 +23,59 @@ class Video:
         self.time_now = time_now
         self.adult_mode = adult_mode
 
-    # def __new__(cls, *args, **kwargs):
-    #     cls.videos.append(args)
-    #     return object.__new__(cls)
+    def __eq__(self, other):
+        return other.title == self.title
     def video_title(self):
         return self.title
 
     def __str__(self):
         return self.title
 
-class UrTube : # доделать проверку на повторы
+class UrTube :
 
     def __init__(self):
         self.users = []
         self.videos = []
         self.current_user = None
-
-
-    # def __new__(cls, *args, **kwargs):
-    #     cls.videos.append(args)
-    #     return object.__new__(cls)
+    def info_users (self):
+        info_us = []
+        for i in self.users:
+            info_us.append(i.nickname)
+        return info_us
     def add (sefl, *args):
           for arg in args:
             if arg.title not in sefl.videos:
                 sefl.videos.append(arg)
 
-            # print(arg)
-
-
-            # if arg.title not in UrTube.videos.__str__() :
-            #     print(UrTube.videos)
-            #     # print(f'Добавлено видео : {arg}')
-            #     UrTube.videos.append(arg)
-
     def get_videos(self, string):
         search_video = []
         for video in self.videos:
-            # print(video.title.lower(), string.lower())
             if string.lower() in video.title.lower():
-                search_video.append(video)
-                print(video)
+                search_video.append(video.title)
+                #print(video)
         return search_video
     def watch_video (self, video_title):
-        # if self.current_user != None and int(self.current_user.age) >=18:
-            for video in self.videos:
-                if video_title == video.title:
-                    for second in range(video.time_now, video.duration):
-                        # print (second)
-                        # time.sleep(1)
-                        print("End of video")
-
-
-
-    def log_in(self, nickname, password, age):
-        user = User(nickname,password,age)
-        if user in self.users:
-            self.current_user = user
-            print(f'Вход пользавателя {user}')
+        if self.current_user :
+            if self.current_user.age < 18:
+                print("Вам нет 18 лет, пожалуйста покиньте страницу")
+            else:
+                for video in self.videos:
+                    if video_title == video.title:
+                        for second in range(video.time_now +1, video.duration+1):
+                            print(f'\r{second:3}', end='')
+                            time.sleep(1)
+                        print(" End of video")
         else:
-            print('Такого нет в списке пользователей! ')
+            print("Войдете в акаунт для того что бы смотреть видео")
+
+
+    def log_in(self, nickname, password):
+        for user in self.users:
+           if (nickname, hash(password)) == user.get_info():
+                self.current_user = user
+                print(f'Вход пользователя: {self.current_user}')
+                return user
+
 
     def log_out (self):
         self.current_user = None
@@ -91,19 +86,10 @@ class UrTube : # доделать проверку на повторы
     def register(self, nickname, password, age): # нужнв проверка на пользователя в базе
         new_user = User(nickname, password, age)
         if new_user not in self.users:
-
             self.users.append(new_user)
             self.current_user = new_user
-            print ("new user  - ", new_user )
         else:
-            print(f'NO!!!{nickname}')
-            self.log_out()
-
-
-
-
-
-
+            print(f'Пользователь {nickname} уже существует')
 
 
 if __name__ == '__main__':
@@ -123,6 +109,7 @@ if __name__ == '__main__':
     ur.register('vasya_pupkin', 'lolkekcheburek', 13)
     ur.watch_video('Для чего девушкам парень программист?')
     ur.register('urban_pythonist', 'iScX4vIJClb9YQavjAgF', 25)
+
     ur.watch_video('Для чего девушкам парень программист?')
 
     # Проверка входа в другой аккаунт
@@ -131,3 +118,6 @@ if __name__ == '__main__':
 
     # Попытка воспроизведения несуществующего видео
     ur.watch_video('Лучший язык программирования 2024 года!')
+    # --------------------------
+    ur.log_out()
+    ur.log_in('urban_pythonist', 'iScX4vIJClb9YQavjAgF')
