de3aaa330d94f481094a4b4c51989bcf134e5717
EvgenyKushvid <nlmontreal24@gmail.com>
2024-12-19 01:02:08 +0300

    Add files via upload

--- module_14_1.py
+++ module_14_1.py
@@ -22,12 +22,12 @@ balance INTEGER NOT NULL
 # for i in range(1,11,3):
 #     cursor.execute(f"DELETE FROM Users WHERE id  ={i}")
 
-cursor.execute("SELECT username, age FROM Users WHERE age != ?", (60,))
+cursor.execute("SELECT username,email, age, balance FROM Users WHERE age != ?", (60,))
 
 users = cursor.fetchall()
 
 for user in users:
-    print(user)
+    print(f'Имя: {user[0]} | Почта:{user[1]} | Возраст:{user[2]} | Баланс:{user[3]} ')
 
 connection.commit()
 connection.close()
